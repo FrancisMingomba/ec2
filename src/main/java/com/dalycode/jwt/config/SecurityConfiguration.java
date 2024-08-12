3b44f07a072dbed182c8db5e36e7d00507d4211c
FrancisMProjects <mingomba@outlook.com>
2024-08-12 17:13:05 -0500

    first commit

---
+++ src/main/java/com/dalycode/jwt/config/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.dalycode.jwt.config;
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfiguration  {
+
+
+}
---
+++ src/test/java/com/dalycode/jwt/JwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalycode.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
