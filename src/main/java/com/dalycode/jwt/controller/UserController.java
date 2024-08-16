74f58f7cca99fb1cf89686aaac2f6f964fb67ad8
FrancisMingomba <mingomba@gmail.com>
2024-08-15 22:55:15 -0500

    Update UserController.java

--- src/main/java/com/dalycode/jwt/controller/UserController.java
+++ src/main/java/com/dalycode/jwt/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/test")
     public String home() {
-        return "It's works";
+        return "It's works again";
     }
 
     @GetMapping("/try")
